e510aa60bd19087575154151dc1276cbb392ab6f
ISTang <joebin.don@gmail.com>
2013-11-15 09:40:04 +0800

    改进对站点中有问题页面的查询支持

--- web/db.js
+++ web/db.js
@@ -381,6 +381,7 @@ function getFailedPages(siteName, includeChildSites, includedUrlString, callback
                     if (err) return callback(err);
                     log("Found " + pages.length + " failed page(s).");
                     for (var i in pages) {
+                        var page = pages[i];
                         result.push({row: parseInt(i)+1, url: page.url, reason: page.reason});
                     }
                     callback();
